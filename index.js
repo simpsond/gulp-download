be6814629c3ca865a685e30914a910bd670aafff
Dustin Simpson <dustin@matrixdm.com>
2017-05-11 05:53:38 -0700

    Now converted ALL logging to gutil.log and code cleanup to get closer to style conformity

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var through = require("through"),
-	gutil = require("gulp-util"),
-	request = require("request"),
-	progress = require("request-progress"),
-	col = gutil.colors,
-	log = gutil.log;
+/* global require, module, Buffer */
+var through = require('through');
+var gutil = require('gulp-util');
+var request = require('request');
+var progress = require('request-progress');
+var col = gutil.colors;
+var log = gutil.log;
 
-module.exports = function(urls){
-	var stream = through(function(file, enc, cb){
+module.exports = function(urls) {
+	var stream = through(function(file, enc, cb) {
 		this.push(file);
 		cb();
 	});
@@ -16,37 +17,37 @@ module.exports = function(urls){
 	var downloadCount = 0;
 
 
-	function download(url){
-		var fileName,
-			firstLog = true;
+	function download(url) {
+		var fileName;
+		var firstLog = true;
 
-		if (typeof url === "object") {
+		if (typeof url === 'object') {
 			fileName = url.file;
 			url = url.url;
 		} else {
 			fileName = url.split('/').pop();
 		}
 		progress(
-			request({url:url,encoding:null}, downloadHandler),
+			request({url: url, encoding: null}, downloadHandler),
 			{throttle: 1000, delay: 1000}
 		)
-		.on('progress',function(state){
-			process.stdout.write(' '+state.percent+'%');
+		.on('progress', function(state) {
+			log(state.percent+'%');
 		})
-		.on('data',function(){
+		.on('data', function() {
 			if(firstLog) {
 				log('Downloading '+col.cyan(url)+'...');
 				firstLog = false;
 			}
 		});
 
-		function downloadHandler(err, res, body){
-			var file = new gutil.File( {path:fileName, contents: new Buffer(body)} );
+		function downloadHandler(err, res, body) {
+			var file = new gutil.File( {path: fileName, contents: new Buffer(body)} );
 			stream.queue(file);
 
-			process.stdout.write(' '+col.green('Done\n'));
+			log(col.green('Done\n'));
 			downloadCount++;
-			if(downloadCount != files.length){
+			if(downloadCount != files.length) {
 				download(files[downloadCount]);
 			}else{
 				stream.emit('end');
